e574a5bc1cda6ec1e3ba185fd6dbca5202ca714a
Agung Gumelar <gunghello@gmail.com>
2025-02-24 02:37:43 +0700

    [MODIFY] Protect Secure Page

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -5,13 +5,15 @@ import Footer from "./Footer";
 const AppLayout = () => {
   return (
     <>
-      <Header />
-      <div className="w-11/12 md:w-3/4 mx-auto min-h-screen">
-        <main className="pt-20">
-          <Outlet />
-        </main>
+      <div className="bg-cover bg-center bg-[url('/bg.jpg')]">
+        <Header />
+        <div className="w-11/12 md:w-3/4 mx-auto min-h-screen">
+          <main className="pt-20">
+            <Outlet />
+          </main>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,52 +1,36 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
-import { Link } from "react-router";
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
 import Logo from "../components/Logo";
-
-const menu = [
-  {
-    title: "Home",
-    link: "/",
-  },
-  {
-    title: "Profile",
-    link: "/profile",
-  },
-];
+import Modal from "../components/Modal";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <header className="fixed w-full flex justify-between items-center bg-blue-500 md:px-10 p-3 z-10">
-        <Logo />
-        <nav id="menu" className="flex gap-x-10">
-          {menu.map((item, index) => (
-            <Link
-              key={index}
-              className="font-semibold text-white"
-              to={item.link}
-            >
-              {item.title}
-            </Link>
-          ))}
-        </nav>
-        <div id="profile">
-          <SignedOut>
-            <SignInButton>
-              <button className="bg-blue-300 rounded-4xl">
-                Custom sign in button
-              </button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+      <header className="fixed w-full  bg-white/70 backdrop-blur-xs py-3 z-10">
+        <div className="w-11/12 md:w-3/4 mx-auto flex justify-between items-center">
+          <Logo />
+          <div id="profile">
+            <SignedOut>
+              <div className="flex gap-x-3">
+                <button
+                  onClick={() => setIsOpen(true)}
+                  className="bg-blue-500  text-white px-4 py-1 font-semibold rounded cursor-pointer transition-all duration-300"
+                >
+                  Login / Register
+                </button>
+              </div>
+            </SignedOut>
+            <SignedIn>
+              <UserButton showName />
+            </SignedIn>
+          </div>
         </div>
       </header>
+
+      <Modal isOpen={isOpen} padding={false} onClose={() => setIsOpen(false)}>
+        <SignIn />
+      </Modal>
     </>
   );
 };
