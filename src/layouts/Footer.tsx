74038bdb096895ee1860a299b2644fc66a18adb5
Agung Gumelar <gunghello@gmail.com>
2025-02-23 23:04:10 +0700

    [ADD] Add Feature Detail Jobs

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <>
-      <footer className="bg-gray-800 text-white text-center p-5">
-        <p>© 2021 All rights reserved</p>
+      <footer className="bg-blue-500 text-white font-semibold text-center p-5">
+        <p>© 2025 All rights reserved</p>
       </footer>
     </>
   );
